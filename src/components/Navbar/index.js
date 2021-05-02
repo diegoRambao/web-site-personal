d7d950c39d2ed481002b836394629acbf0b6b0b2
Diego RJ <diegoandrestrom04@gmail.com>
2021-05-02 14:37:54 -0500

    feat: create button toggle theme

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import logo from '../../static/images/logo.svg'
 import { Nav, NavbarBrand, NavbarImg, NavbarItems, ItemNav } from './style'
+import { ToggleTheme } from '../ToggleTheme'
+import logo from '../../static/images/logo.svg'
 
 export const Navbar = () => {
    return (
@@ -8,12 +9,15 @@ export const Navbar = () => {
          <div className="container">
             <NavbarBrand>
                <NavbarImg src={logo} alt="Logo brand" loading="lazy" />
-               <NavbarItems>
-                  <ItemNav href="/home">Home</ItemNav>
-                  <ItemNav href="/about">About me</ItemNav>
-                  <ItemNav href="/project">Projects</ItemNav>
-                  <ItemNav href="/contact">Contact</ItemNav>
-               </NavbarItems>
+               <div style={{ display: 'flex' }}>
+                  <ToggleTheme />
+                  <NavbarItems>
+                     <ItemNav href="/home">Home</ItemNav>
+                     <ItemNav href="/about">About me</ItemNav>
+                     <ItemNav href="/project">Projects</ItemNav>
+                     <ItemNav href="/contact">Contact</ItemNav>
+                  </NavbarItems>
+               </div>
             </NavbarBrand>
          </div>
       </Nav>
---
+++ src/components/ToggleTheme/style.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+export const ContainerTheme = styled.label`
+   margin-right: 30px;
+   .toggle-checkbox {
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+      height: 0;
+      width: 0;
+   }
+
+   .toggle-slot {
+      position: relative;
+      height: 30px;
+      width: 62px;
+      border: 1px solid #e4e7ec;
+      border-radius: 10em;
+      background-color: white;
+      transition: background-color 250ms;
+   }
+
+   .toggle-checkbox:checked ~ .toggle-slot {
+      background-color: #374151;
+   }
+
+   .toggle-button {
+      transform: translate(5px, 5px);
+      position: absolute;
+      height: 20px;
+      width: 20px;
+      border-radius: 50%;
+      background-color: #ffeccf;
+      box-shadow: inset 0px 0px 0px 0.75em #ffbb52;
+      transition: background-color 250ms, border-color 250ms, transform 500ms cubic-bezier(0.26, 2, 0.46, 0.71);
+   }
+
+   .toggle-checkbox:checked ~ .toggle-slot .toggle-button {
+      background-color: #485367;
+      box-shadow: inset 0px 0px 0px 0.75em white;
+      transform: translate(35px, 5px);
+   }
+
+   .toggle-checkbox:checked ~ .toggle-slot .moon-icon-wrapper {
+      opacity: 1;
+      transform: translate(35px, 5px) rotate(-15deg);
+   }
+`
