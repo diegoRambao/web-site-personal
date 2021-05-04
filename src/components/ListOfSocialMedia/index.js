c52570ec5fb5ef81a6021d76ea2375e53460532f
Diego RJ <diegoandrestrom04@gmail.com>
2021-05-04 13:32:56 -0500

    feat: add links social media

--- src/components/ListOfSocialMedia/index.js
+++ src/components/ListOfSocialMedia/index.js
@@ -5,16 +5,16 @@ import { Twitter, Instagram, Linkedin, GitHub } from 'react-feather'
 export function ListOfSocialMedia({ color }) {
    return (
       <ContainerSocial>
-         <LinkSocial href="/project" color={color}>
+         <LinkSocial href="https://twitter.com/DiegoRambao" color={color} target="_blank">
             <Twitter />
          </LinkSocial>
-         <LinkSocial href="/project" color={color}>
+         <LinkSocial href="https://www.linkedin.com/in/diego-rambao/" color={color} target="_blank">
             <Linkedin />
          </LinkSocial>
-         <LinkSocial href="/project" color={color}>
+         <LinkSocial href="https://github.com/diegoRambao" color={color} target="_blank">
             <Instagram />
          </LinkSocial>
-         <LinkSocial href="/project" color={color}>
+         <LinkSocial href="https://www.instagram.com/diegorambao/" color={color} target="_blank">
             <GitHub />
          </LinkSocial>
       </ContainerSocial>
